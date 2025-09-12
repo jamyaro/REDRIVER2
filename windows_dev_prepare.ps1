ddfee61981c661dd67626c5a746b0d2ec2584138
SoapyMan <ishurumov@gmail.com>
2025-09-12 22:51:33 +0500

    - try fix CI build on Windows

--- windows_dev_prepare.ps1
+++ windows_dev_prepare.ps1
@@ -1,7 +1,10 @@
+$openal_ver = '1.23.1'
+$sdl2_ver = '2.30.2'
+
 $windows_premake_url = 'https://github.com/premake/premake-core/releases/download/v5.0.0-beta1/premake-5.0.0-beta1-windows.zip'
 $windows_jpeg_url = 'http://www.ijg.org/files/jpegsr9d.zip'
-$windows_openal_url = 'https://openal-soft.org/openal-binaries/openal-soft-1.21.1-bin.zip'
-$windows_sdl2_url = 'https://www.libsdl.org/release/SDL2-devel-2.0.20-VC.zip'
+$windows_openal_url = 'https://github.com/kcat/openal-soft/releases/download/' + $openal_ver + '/openal-soft-' + $openal_ver + '-bin.zip'
+$windows_sdl2_url = 'https://github.com/libsdl-org/SDL/releases/download/release-' + $sdl2_ver + '/SDL2-devel-' + $sdl2_ver + '-VC.zip'
 
 $project_folder = '.\\src_rebuild'
 $dependency_folder = $project_folder + '\\dependencies'
@@ -21,8 +24,8 @@ Expand-Archive JPEG.zip -DestinationPath $dependency_folder
 
 # Generate project files
 $windows_jpeg_dir = '.\\dependencies\\jpeg-9d'
-$windows_openal_dir = '.\\dependencies\\openal-soft-1.21.1-bin'
-$windows_sdl2_dir = '.\\dependencies\\SDL2-2.0.20'
+$windows_openal_dir = '.\\dependencies\\openal-soft-' + $openal_ver + '-bin'
+$windows_sdl2_dir = '.\\dependencies\\SDL2-' + $sdl2_ver
 
 $env:SDL2_DIR = $windows_sdl2_dir
 $env:OPENAL_DIR = $windows_openal_dir
